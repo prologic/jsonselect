f47cac3bb945bebac5e8943db87a3d237205a9df
James Mills <j.mills@griffith.edu.au>
2014-06-05 14:14:42 +1000

    Run the level 3 tests but skip them

--- tests/test_conformance.py
+++ tests/test_conformance.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 
-from pytest import raises
+from pytest import raises, skip
 from py.path import local as localpath
 
 
@@ -17,30 +17,35 @@ def pytest_generate_tests(metafunc):
     argvalues = []
     ids = []
 
-    for level in ("level_{0:d}".format(level) for level in [1, 2, 3]):
-        if level == "level_3":
-            continue
-
+    for level in range(1, 4):
         test_path = root_path.join(
-            "..", "conformance_tests", "upstream", level
+            "..", "conformance_tests", "upstream", "level_{0:d}".format(level)
         )
 
         tests = list(get_tests(test_path))
-        argvalues.extend([test[:3] for test in tests])
-        ids.extend(["{0:s}_{1:s}".format(level, test[-1]) for test in tests])
+        argvalues.extend([(level,) + test[:3] for test in tests])
+        ids.extend(
+            [
+                "level_{0:d}_{1:s}".format(level, test[-1])
+                for test in tests
+            ]
+        )
 
     test_path = root_path.join(
         "..", "conformance_tests", "custom"
     )
 
     tests = list(get_tests(test_path))
-    argvalues.extend([test[:3] for test in tests])
+    argvalues.extend([(None,) + test[:3] for test in tests])
     ids.extend(["custom_{0:s}".format(test[-1]) for test in tests])
 
     metafunc.parametrize(metafunc.fixturenames, argvalues, ids=ids)
 
 
-def test(selector, document, output):
+def test(level, selector, document, output):
+    if level == 3:
+        skip("Level 3 not implemented!")
+
     parser = Parser(document)
 
     try:
